f7093464a95d12c218231a099bc27497a1257025
Drew Bragg <DRBragg@gmail.com>
2026-02-11 21:50:01 -0500

    feat: Add `units` field to Cookware and update canonical.yml

--- lib/cooklang_rb/cookware.rb
+++ lib/cooklang_rb/cookware.rb
@@ -1,18 +1,22 @@
 # frozen_string_literal: true
 
 require_relative "tag"
+require_relative "with_units"
 
 module CooklangRb
   class Cookware < Tag
-    attr_reader :name, :quantity
+    include WithUnits
+
+    attr_reader :name, :quantity, :units
 
     def self.tag
       COOKWARE_TAG
     end
 
-    def initialize(name:, quantity: 1)
+    def initialize(name:, quantity: 1, units: "")
       @name = clean_name(name)
       @quantity = resolve_quantity(quantity, default: 1)
+      @units = clean_units(units)
     end
   end
 end
